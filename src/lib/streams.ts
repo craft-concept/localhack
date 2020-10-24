cf62d9f779d5f097ec27ae742061b7b1638c1a7f
Jeff Peterson <jeff@yak.sh>
2020-10-24 13:32:29 -0700

    Add hareactive

---
+++ src/lib/streams.ts
@@ -0,0 +1,29 @@
+import { Stream, producerStream } from "@funkia/hareactive"
+
+export { Stream, producerStream as produce }
+
+export type Listener<Args extends any[]> = (...args: Args) => void
+
+export interface EventMap {
+  [eventName: string]: any[]
+}
+
+export interface Emitter<T extends EventMap> {
+  on<K extends keyof T>(name: K, cb: Listener<T[K]>): this
+  off<K extends keyof T>(name: K, cb: Listener<T[K]>): this
+}
+
+export function eventArgsStream<T extends EventMap, K extends keyof T>(
+  target: Emitter<T>,
+  name: K,
+): Stream<T[K]> {
+  return producerStream(push => {
+    const onEvent = (...args: T[K]) => push(args)
+
+    target.on(name, onEvent)
+
+    return () => {
+      target.off(name, onEvent)
+    }
+  })
+}
