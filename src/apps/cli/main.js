aa679a326d79adc46bc78afaf8465988f16712a8
Jeff Peterson <jeff@yak.sh>
2020-12-02 12:40:57 -0800

    Remove legacy src/apps/

--- src/apps/cli/main.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-require("ts-node").register()
-require("../../hacks")
-
-const { make } = require("../../lib")
-const store = make(require("./main.ts"))
-
-store.dispatch({ type: "Start" })
--- src/apps/ui/main.js
+++
@@ -1,8 +0,0 @@
-require("ts-node").register()
-require("../../hacks")
-const Path = require("path")
-
-const { make } = require("../../lib")
-make(require("./main.ts"))
-
-navigator.serviceWorker.register(Path.join(__dirname, "compiler.js"))
