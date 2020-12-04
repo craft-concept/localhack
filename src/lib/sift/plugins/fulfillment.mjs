38a72ba76e9f982619715c0027176b20f2dba4a0
Jeff Peterson <jeff@yak.sh>
2020-12-03 18:56:09 -0800

    Only cache inputs that are indexed

--- src/lib/sift/plugins/fulfillment.mjs
+++
@@ -1,4 +0,0 @@
-export function requestQueue(input) {
-  if (typeof input.onResponse !== 'function') return
-  for (const req of iter)
-}
--- src/lib/sift/plugins/memory.mjs
+++ src/lib/sift/plugins/memory.mjs
@@ -12,43 +12,74 @@ export const acceptIndexes = input => state => {
 }
 
 /** Use our recorded indexes to find ids for id-less inputs. */
-export const findId = input => state => {
+export function findId(input) {
   if (input.id) return
 
-  for (const [name, indexer] of entries(state.indexers)) {
-    const index = state[name]
-    if (!index) return
+  return state => {
+    for (const [name, indexer] of entries(state.indexers)) {
+      const index = state[name]
+      if (!index) return
 
-    for (const key of iter(indexer(input))) {
-      if (index[key]) {
-        input.id = index[key]
-        return
+      for (const key of iter(indexer(input))) {
+        if (index[key]) {
+          input.id = index[key]
+          return
+        }
       }
     }
   }
 }
 
+/**
+ * Update our cache with data from the input and fill in the input's properties
+ * with values from the cache.
+ */
 export const populateFromId = input => state => {
-  state.byId ??= {}
-
-  input.id ??= uuid()
-  input.createdAt ??= new Date().toISOString()
-
-  const cache = (state.byId[input.id] ??= {})
-  deepAssign(cache, current(input))
-  deepAssign(input, current(cache))
-}
-
-export const writeIndexes = input => state => {
   if (!input.id) return
 
+  state.byId ??= {}
+  const cached = state.byId[input.id]
+
+  if (cached) {
+    deepAssign(cached, current(input))
+    deepAssign(input, current(cached))
+  }
+}
+
+/**
+ * Index the current input using our indexers. If the input can be indexed and
+ * has no id, then it is given one.
+ */
+export const writeIndexes = input => state => {
   for (const [name, indexer] of entries(state.indexers)) {
     state[name] ??= {}
 
     for (const key of iter(indexer(input))) {
+      input.id ??= uuid()
+      input.createdAt ??= new Date().toISOString()
+
       state[name][key] = input.id
     }
   }
 }
 
-export default [acceptIndexes, findId, populateFromId, writeIndexes]
+/**
+ * Cache any input that has an id.
+ */
+export function writeToCache(input) {
+  if (!input.id) return
+
+  state.byId ??= {}
+  const cached = (state.byId[input.id] ??= {})
+
+  deepAssign(cached, current(input))
+  deepAssign(input, current(cached))
+}
+
+export default [
+  acceptIndexes,
+  findId,
+  populateFromId,
+  writeIndexes,
+  writeToCache,
+]
