338e9ab75d217a0e8caacaf1fb8bee70fb34c38f
Jeff Peterson <jeff@yak.sh>
2021-02-25 20:10:21 -0800

    Fiddle with Queue

--- src/lib/Queue.mjs
+++ src/lib/Queue.mjs
@@ -1,3 +1,5 @@
+import { Enum } from "./Enum"
+
 class QueueLockError extends Error {
   constructor(name) {
     super(`This Queue${name && ` (${name})`} is already locked.`)
@@ -10,9 +12,23 @@ export class Queue {
     this.push = (...items) => (this.q.push(...items), this)
   }
 
-  async *[Symbol.asyncIterator]() {
+  *[Symbol.iterator]() {
     if (this.locked) throw new QueueLockError()
     this.locked = true
     while (this.q.length) yield this.q.shift()
+    this.locked = false
+  }
+
+  async *[Symbol.asyncIterator]() {
+    yield* this
   }
 }
+
+Queue.test?.(({ eq }) => {
+  const q = new Queue()
+
+  q.push(1, 2, 3)
+
+  eq([...q], [1, 2, 3])
+  eq([...q], [])
+})
