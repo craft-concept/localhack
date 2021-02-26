bc58681ebfc0f8aa4937480c0e267b04a80c8f56
Jeff Peterson <jeff@yak.sh>
2021-02-25 20:08:05 -0800

    Simply Fold and use ReplySet

--- src/lib/Fold.mjs
+++ src/lib/Fold.mjs
@@ -1,49 +1,39 @@
 import { Enum, iter } from "./Enum"
 import { edit, current } from "./edit"
+import { ReplySet } from "./ReplySet"
 
-export function make() {
+export function make(state = {}) {
   function self(...inputs) {
-    for (const input of iter(inputs)) {
-      self.send(input).each(x => x.then?.())
-    }
+    return self.send(...inputs)
   }
 
-  Object.assign(self, {
-    self,
-    state: {},
-    *send(input) {
-      edit(input, input => {
-        self.state = edit(self.state, state => {
-          state.plugins ??= []
+  self.state = state
+  self.self = self
+  self.send = function send(...inputs) {
+    const replies = new ReplySet(self.send)
+    self.state = edit(self.state, state => {
+      for (const input of iter(inputs)) self.mutate(input, state, replies)
+    })
 
-          if (typeof input == "function") state.plugins.push(input)
-          if (typeof input.with === "function") state.plugins.push(input.with)
+    return replies
+  }
 
-          runAll(state.plugins, input, state, self)
-        })
-      })
-    },
-  })
+  self.mutate = function mutate(input, state, replies) {
+    if (typeof input == "function") {
+      input.key ??= input.name || "anon"
+      state.plugins.push(input)
+    }
+
+    for (const plugin of state.plugins) {
+      replies.add(plugin.call(state[plugin.key], input, state))
+    }
+  }
 
   return self
 }
 
-export function runAll(fns, input, state, self) {
-  return Enum.of(fns).flatMap(run(input, state, self)).array
-}
-
-export function run(input, state, self) {
-  return fn => {
-    const key = fn.key || fn.name || "anon"
-    const local = (state[key] ??= {})
-    const replies = fn.call(local, input, state, self)
-
-    return Enum.of(replies).selectMap(reply =>
-      typeof reply == "function" ? reply.call(local, self.send) : reply,
-    )
-  }
-}
-
-runAll.test?.(({ eq }) => {
-  eq(runAll([(a, b) => a + b], 1, 2), runAll([(a, b) => a - b], 1, 2), [3, -1])
+make.test?.(({ eq }) => {
+  const self = make()
+  self(() => ({ x: 1 }))
+  eq([...self({})], [{ x: 1 }])
 })
--- src/lib/ReplySet.mjs
+++ src/lib/ReplySet.mjs
@@ -1,5 +1,22 @@
 import { Enum } from "./Enum"
 
 export class ReplySet extends Enum {
-  then(fn, catchFn) {}
+  constructor(send) {
+    super(() => this.replies)
+    this.send = send
+    this.replies = []
+    this.errors = []
+  }
+
+  add(replies) {
+    for (const reply of iter(replies)) {
+      if (typeof reply == "function") this.add(reply(this.send))
+      else if (typeof reply.then == "function")
+        reply.then(
+          r => this.add(r),
+          x => this.errors.push(x),
+        )
+      else this.replies.push(reply)
+    }
+  }
 }
