7998e32f4d21e8f77446994a88cff0a00274952c
Jeff Peterson <jeff@yak.sh>
2021-03-25 12:24:05 -0700

    Add async messages for transforming state

--- src/lib/Transmute.mjs
+++ src/lib/Transmute.mjs
@@ -1,33 +1,50 @@
 import { Enum, iter } from "lib/Enum"
 
 export class Transmute {
-  constructor(plugins = []) {
+  constructor(plugins = [], state = {}) {
     this.plugins = plugins
+    this.state = state
   }
 
-  transform(input, ctx = {}) {
-    return Enum.of(this.walk(ctx, input))
+  transform(input) {
+    return Enum.of(this.walk(this.state, input))
   }
 
-  *walk(parentCtx, input, addCtx = {}) {
+  /**
+   * Walk a level down
+   */
+  walkDown(parentCtx, input, addCtx = {}) {
     if (input == null) return
 
     const ctx = Object.assign(Object.create(parentCtx), addCtx)
-    const recur = this.walk.bind(this, ctx)
+
+    return this.walk(ctx, input)
+  }
+
+  *walk(ctx, input) {
+    if (input == null) return
+
+    const recur = this.walkDown.bind(this, ctx)
+    const asyncFns = []
 
     for (const node of iter(input)) {
       // Mutate the current node
       for (const plugin of this.plugins) {
         // console.log("walking with", plugin.name, node)
-        yield* iter(plugin.call(ctx, node, recur))
+        const returns = yield* iter(plugin.call(ctx, node, recur))
+        asyncFns.push(returns)
       }
     }
+
+    const send = this.transform.bind(this)
+
+    for (const fn of iter(asyncFns)) fn(send)
   }
 }
 
 Transmute.test?.(({ eq }) => {
   const tm = new Transmute([Emphasis, Button, AsciiTags, AsciiText])
-  const tx = n => tm.transform(n).join("")
+  const tx = n => tm.transform(n).join()
 
   eq(tx({ Button: true, children: "Hello" }), "[Hello]")
   eq(
