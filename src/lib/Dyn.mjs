5ee4f0840877fab93f10582e6ba1eb04694b3cfa
Jeff Peterson <jeff@yak.sh>
2021-05-07 19:29:04 -0700

    Add this back to Dyn

--- src/lib/Dyn.mjs
+++ src/lib/Dyn.mjs
@@ -4,51 +4,69 @@ import { T, match } from "lib/patterns"
 export { T }
 
 export class MissingPatternError extends Error {
-  constructor(name, args) {
-    super(`Missing pattern for ${name}(${args.map(JSON.stringify).join(", ")})`)
+  constructor(name, this_, args) {
+    super(
+      `Missing pattern for ${name}.call(${this_}, ${args
+        .map(JSON.stringify)
+        .join(", ")})`,
+    )
     this.name = name
+    this.this = this_
     this.args = args
   }
 }
 
-export default function dynFn(name) {
+export default function Dyn(name) {
   function self(...args) {
     return self.invoke.apply(self, args)
   }
 
   return fnWith(
     {
+      this: T.Any,
       name,
       patterns: [],
-      invoke(...args) {
-        for (let fn of self.patterns) {
-          if (match(fn.inputs)(args)) {
-            return fn.apply(this, args)
-          }
-        }
 
-        throw new MissingPatternError(self.name, args)
+      invoke(...args) {
+        for (let x of self.all.apply(this, args)) return x
+        throw new MissingPatternError(self.name, this, args)
+      },
+
+      *all(...args) {
+        for (let fn of self.patterns) {
+          if (match(fn.this)(this) && match(fn.inputs)(args))
+            yield fn.apply(this, args)
+        }
+      },
+
+      withThis(thisShape) {
+        self.this = thisShape
+        return self
+      },
+
+      withoutThis() {
+        self.this = T.Any
+        return self
       },
 
       accept(...inputs) {
         let fn = inputs.pop()
-        this.patterns.unshift(fnWith({ inputs }, fn))
-        return this
+        self.patterns.unshift(fnWith({ this: self.this, inputs }, fn))
+        return self
       },
 
       fallback(...inputs) {
         let fn = inputs.pop()
-
-        this.patterns.push(fnWith({ inputs }, fn))
-        return this
+        self.patterns.push(fnWith({ this: self.this, inputs }, fn))
+        return self
       },
     },
     self,
   )
 }
 
-dynFn.test?.(({ eq, throws }) => {
-  let inspect = dynFn("inspect")
+Dyn.test?.(({ eq, throws }) => {
+  let inspect = Dyn("inspect")
     .accept(String, str => `str: ${str}`)
     .accept(Number, num => `num: ${num}`)
 
@@ -63,4 +81,14 @@ dynFn.test?.(({ eq, throws }) => {
 
   eq(inspect(1), "num: 1")
   eq(inspect({}), "any: {}")
+  eq([...inspect.all(1)], ["num: 1", "any: 1"])
+
+  inspect
+    .withThis(String)
+    .accept(Number, function (num) {
+      return `${this}: ${num}`
+    })
+    .withoutThis()
+
+  eq(inspect.call("tag", 42), "tag: 42")
 })
--- src/lib/Precursor.mjs
+++ src/lib/Precursor.mjs
@@ -105,6 +105,7 @@ export default def
           })
         }
       }
+      return this
     },
 
     tap(fn) {
